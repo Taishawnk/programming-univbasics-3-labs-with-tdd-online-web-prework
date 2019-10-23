20cd01c12b2f902f2c58697c58b5812112044912
Taishawn King <taishawnking1@yahoo.com>
2019-10-23 22:33:58 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -3,18 +3,16 @@ second_number = 1
 sum = first_number + second_number
 difference = first_number - second_number
 product = first_number * second_number
-quotient = first_number /
+quotient = first_number / second_number
 
 
 
 
 
 
-- The third test is looking for a local variable named `sum`. The `sum` variable
-  is the result of adding `first_number` and `second_number` together. This test
-  is using all three variables. Not only that, the test is using whatever values
-  _you_ assigned to `first_number` and `second_number`.
+#- The third test is looking for a local variable named `sum`. The `sum` variable
+  #is the result of adding `first_number` and `second_number` together. This test
+ # is using all three variables. Not only that, the test is using whatever values
+  #_you_ assigned to `first_number` and `second_number`.
 
-- The fourth, fifth and sixth tests are similar to the tests for `sum`. Create
-  the variable `difference` for subtracting, `product` for multiplying, and
-  `quotient` for dividing the `first_number` and `second_number` variables.
+#`quotient` for dividing the `first_number` and `second_number` variables.
